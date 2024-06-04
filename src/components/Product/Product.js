1a22c6ee193c2b67c06f10672aadc81ccd7d55a0
Sheyda Rajaee <sheydarajaee@Sheydas-MacBook-Pro.local>
2024-06-05 01:42:38 +0330

    secod commit

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const data =[{"name":"test1"},{"name":"test2"}];
+const listItems = data.map((d) => <li key={d.name}>{d.name}</li>);
+
 
 const Product = () => (
   <div>
     Product 
+    {listItems }
   </div>
 );
 
